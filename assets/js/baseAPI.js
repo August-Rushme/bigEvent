f263a82116c7120a15d025b0e8b5b31da3edd0ca
August <864011713@qq.com>
2021-03-11 19:27:09 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,24 @@
 $.ajaxPrefilter((options) => {
   options.url = " http://ajax.frontend.itheima.net" + options.url;
-  console.log(options.url);
+
+  //统一为有权限的接口设置headers
+  if (options.url.indexOf("/my/") !== -1) {
+    options.headers = {
+      Authorization: localStorage.getItem("token") || "",
+    };
+  }
+  //全局统一挂载complete回调函数
+  options.complete = (res) => {
+    //complete回调函数中，可以使用res.responseJSON
+    //拿到服务器响应回来的数据
+    if (
+      res.responseJSON.status === 1 &&
+      res.responseJSON.message === "身份认证失败！"
+    ) {
+      //强行清空token
+      localStorage.removeItem("token");
+      //强制跳转到登录页
+      location.href = "./login.html";
+    }
+  };
 });
